77484506c75535479d0c8ac2592e00c316b2c0b1
carlos-edhr <carlos.edhr@protonmail.com>
2021-04-19 00:29:16 -0700

    Created abstract ItemBase model and Text, File, Image, Video models for diverse content

--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
-from django.contenttypes.models import ContentType 
-from django.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType 
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -55,7 +55,33 @@ class Content(models.Model):
     object_id = models.PositiveIntegerField()
     item= GenericForeignKey('content_type', 'object_id')
 
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, 
+                            related_name='%(class)s_related',
+                            on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta: 
+        abstract = True
     
+    def __str__(self):
+        return slef.title
+ 
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
 
 
     
