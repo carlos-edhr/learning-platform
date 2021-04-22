fc8536c6c53f8c8548402d4d604c3a1c62ee3c24
carlos-edhr <carlos.edhr@protonmail.com>
2021-04-21 18:32:40 -0700

    Added views to display Subjects and courses in landing page

--- courses/views.py
+++ courses/views.py
@@ -10,6 +10,48 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps 
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count 
+from .models import Subject 
+from django.views.generic.detail import DetailView 
+
+class CourseDetailView(DetailView):
+    model = Course 
+    template_name = 'courses/course/detail.html'
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course 
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None ): 
+        subjects = Subject.objects.annotate(
+                        total_courses=Count('courses'))
+        courses =  Course.objects.annotate(
+                        total_modules=Count('modules'))
+        if subject: 
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject, 
+                                        'courses': courses})
+
+class ModuleOrderView(CsrfExemptMixin, 
+                    JsonRequestResponseMixin, 
+                    View):
+        def post(self, request):
+            for id, order in self.request_json.items():
+                Module.objects.filter(id=id, 
+                        course__owner=request.user).update(order=order)
+            return self.render_json_response({'saved': 'OK'}) 
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, 
+                        View): 
+        def post(self, request):
+            for id, order in self.request_json.items():
+                Content.objects.filter(id=id,
+                            module__course__owner=request.user).update(order=order)
+            return self.render_json_response({'saved': 'OK'})
+
 
 class ModuleContentListView(TemplateResponseMixin, View):
     template_name = 'courses/manage/module/content_list.html'
